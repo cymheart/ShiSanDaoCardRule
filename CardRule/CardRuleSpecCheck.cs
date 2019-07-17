6783249c7a6b00614bce9948dc836f21c26c07e6
cymheart <2072820700@qq.com>
2019-07-18 01:46:27 +0800

    ok

--- CardRule/CardRuleSpecCheck.cs
+++ CardRule/CardRuleSpecCheck.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Collections.Generic;
+
 namespace CardRuleNS
 {
     /// <summary>
@@ -33,9 +35,12 @@ namespace CardRuleNS
             }
 
             int idx;
-            for (int i=1; i < 13; i++)
+            for (int i=2; i < 14; i++)
             {
-                idx = FindCard(cards, 1);
+                if (i == 14)
+                    i = 1;
+
+                idx = FindCard(cards, i);
                 if(idx == -1)
                 {
                     laiziCount--;
@@ -52,6 +57,80 @@ namespace CardRuleNS
 
             return true;
         }
+
+        /// <summary>
+        /// 判断是否为三同花
+        /// </summary>
+        /// <param name="cards"></param>
+        /// <param name="laiziCount"></param>
+        /// <param name="outFaceValues"></param>
+        /// <returns></returns>
+        bool IsSanTongHua(CardInfo[] cards, int laiziCount, RulePukeFaceValue[] outFaceValues)
+        {
+            List<CardInfo>[] suitCards = new List<CardInfo>[]
+            {
+                new List<CardInfo>(),new List<CardInfo>(),
+                new List<CardInfo>(),new List<CardInfo>(),
+            };
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                suitCards[cards[i].suit].Add(cards[i]);
+            }
+
+            int[] idx = new int[4];
+            int count = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (suitCards[i].Count > 0)
+                    idx[count++] = i;
+            }
+
+
+            if (count == 4)
+                return false;
+
+            if(count == 3)
+            {
+                int min = 4;
+                int minIdx = 0;
+                for (int i=0; i<3; i++)
+                {
+                    if (suitCards[idx[i]].Count > 5)
+                        return false;
+
+                    if (suitCards[idx[i]].Count < min)
+                    {
+                        min = suitCards[idx[i]].Count;
+                        minIdx = idx[i];
+                    }
+                }
+
+                if (min > 3)
+                    return false;
+
+                int n = 0;
+
+                for(int i=0; i < suitCards[minIdx].Count; i++)
+                    outFaceValues[n++] = GetRulePukeFaceValue(suitCards[minIdx][i].value, suitCards[minIdx][i].suit);
+
+                for (int i = suitCards[minIdx].Count; i < 3; i++)
+                {
+                    laiziCount--;
+                    if (laiziCount < 0)
+                        return false;
+
+                    outFaceValues[n++] = RulePukeFaceValue.Laizi;
+                }
+
+            }
+
+     
+
+            return true;
+        }
+
+
 
         int FindCard(CardInfo[] cards, int value)
         {
